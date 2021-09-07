aef8dd517fd271d5b3e45d8a5fcdab03d3dab1bc
Con <62197403+inacceptable@users.noreply.github.com>
2021-09-07 15:49:08 +1000

    Update settings.py

--- covidwebsite/settings.py
+++ covidwebsite/settings.py
@@ -75,9 +75,8 @@ WSGI_APPLICATION = 'covidwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
