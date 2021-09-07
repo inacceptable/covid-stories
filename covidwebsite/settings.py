e6d6e65c061663229df9704caab64311c583d975
Con <62197403+inacceptable@users.noreply.github.com>
2021-09-07 15:43:17 +1000

    ojk

--- covidwebsite/settings.py
+++ covidwebsite/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = 'covidwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'].update(db_from_env)
-
+DATABASES['default']=db_from_env
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
